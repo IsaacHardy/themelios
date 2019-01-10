dbb9b9a11a06e7dea16c10c42416fe777c4a1c9b
Isaac Hardy <isaac.hardy@newspring.cc>
2019-01-10 16:02:34 -0500

    idk

--- api.py
+++ api.py
@@ -25,5 +25,5 @@ def feed():
 
             return jsonify({'trace': traceback.format_exc()})
 
-
-app.run(port=12345, host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(port=12345, host='0.0.0.0', debug=True)
